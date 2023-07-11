effe4fd069cdcb1321ece3f83c9a0edbef953458
Riyaz Ahamed <riyasclassic.ahamed@gmail.com>
2023-07-11 05:44:45 -0400

    removed unwanted code

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -1,4 +1,4 @@
-git# Importing required libraries
+# Importing required libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -10,45 +10,62 @@ driver = webdriver.Chrome()
 driver.get("https://www.amazon.ca")
 time.sleep(3)
 
-# Finding the search bar and entering text
-# search_bar = driver.find_element_by_id("id","twotabsearchtextbox") old syntax
-search_bar = driver.find_element("id","twotabsearchtextbox")
-search_bar.send_keys("laptop")
+# Navigating to the Amazon.ca homepage
+driver.get("https://www.linkedin.com")
+time.sleep(5)
 
+# Finding the username and entering text
+
+username = driver.find_element("id", "session_key")
+username.send_keys("riyazahamed934@gmail.com")
+time.sleep(2)
+
+# Finding the username and entering text
+password = driver.find_element("id", "session_password")
+password.send_keys("riyazahamed@2016")
+time.sleep(2)
+# Click on Sign in button after entering user name and password
+signin = driver.find_element("xpath","//*[@id='main-content']/section[1]/div/div/form[1]/div[2]/button")
+signin.click()
+time.sleep(6)
+
+#search the value in the search bar
+search_bar = driver.find_element("xpath","//*[@id='global-nav-typeahead']/input")
+search_bar.send_keys("hiring")
 # Submitting the search query
 search_bar.send_keys(Keys.RETURN)
-
-# Waiting for the search results page to load
+# Waiting for the search result page to load
 time.sleep(5)
 
-# Verifying that the search results page has loaded
-assert "laptop" in driver.title
+#click on My Network icon
+mynetwork = driver.find_element("xpath","//*[@id='global-nav']/div/nav/ul/li[2]/a/span")
+mynetwork.click()
+# Waiting for the Network page to load
+time.sleep(4)
 
-# Selecting a laptop from the search results
-laptop_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/span/a/div/img")
-# laptop_link = driver.find_element("By.CSS_SELECTOR","span[data-component-type='s-product-image'] a")
-laptop_link.click()
+#click on Jobs
+myjobs = driver.find_element("xpath", "//*[@id='global-nav']/div/nav/ul/li[3]/a/span")
+myjobs.click()
+# Waiting for the Job page to load
+time.sleep(4)
+#Get the title of the Job page
+title = driver.title
+print("Title:", title)
 
+#Verifying that the job page has loaded
+assert "Jobs | LinkedIn" in driver.title
 
-# Waiting for the laptop details page to load
-time.sleep(5)
+#click on messaging
+message = driver.find_element("xpath","//*[@id='global-nav']/div/nav/ul/li[4]/a/span")
+message.click()
+# Waiting for the messaging page to load
+time.sleep(3)
 
-# Adding the laptop to the cart
-add_to_cart_button = driver.find_element("id","add-to-cart-button")
-add_to_cart_button.click()
-
-# Waiting for the cart to update
-time.sleep(5)
-
-# Clicking on no thanks button
-# no_thanks_button= driver.find_element("xpath","/html/body/div[5]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
-# no_thanks_button.click()
-# time.sleep(2)
-
-# Verifying that the laptop has been added to the cart
-cart_count = driver.find_element("id","nav-cart-count")
-assert cart_count.text == "1"
-cart_count.click()
+#click on my notification
+notification = driver.find_element("xpath","//*[@id='global-nav']/div/nav/ul/li[5]/a/span")
+notification.click()
+# Waiting for the Notification page to load
+time.sleep(3)
 
 # Closing the webdriver
 driver.close()
